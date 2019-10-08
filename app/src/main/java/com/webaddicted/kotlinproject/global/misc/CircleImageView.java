8ed7ae1c6d9ed0cf8e42b419ee8e8d9dd672c47e
Deepak Sharma <deepaksharma040695>
2019-10-08 10:45:25 +0530

    first commit

--- app/src/main/java/com/webaddicted/kotlinproject/global/misc/CircleImageView.java
+++ app/src/main/java/com/webaddicted/kotlinproject/global/misc/CircleImageView.java
@@ -34,7 +34,7 @@ import androidx.appcompat.widget.AppCompatImageView;
 import com.webaddicted.kotlinproject.R;
 
 //@SuppressLint("AppCompatCustomView")
-public class CircleImageView extends ImageView {
+public class CircleImageView extends AppCompatImageView {
 
     private static final ScaleType SCALE_TYPE = ScaleType.CENTER_CROP;
 
